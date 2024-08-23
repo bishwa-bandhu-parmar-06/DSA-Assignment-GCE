48edba204af60fdbed86c51f479f152ada3dfa90
BISHWA BANDHU PARMAR <bishwabandhuparmar06@gmail.com>
2024-08-23 09:04:01 +0530

    Add files via upload

---
+++ DSA-Assignment/BubbleSortandSelectionSort.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+// Function to perform Bubble Sort
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap arr[j] and arr[j + 1]
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+// Function to perform Selection Sort
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        // Swap arr[i] and arr[minIndex]
+        int temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+}
+
+// Function to print an array
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    // Bubble Sort
+    printf("Array before Bubble Sort:\n");
+    printArray(arr, n);
+    bubbleSort(arr, n);
+    printf("Array after Bubble Sort:\n");
+    printArray(arr, n);
+
+    // Selection Sort
+    int arr2[] = {29, 10, 14, 37, 13};
+    int m = sizeof(arr2) / sizeof(arr2[0]);
+
+    printf("\nArray before Selection Sort:\n");
+    printArray(arr2, m);
+    selectionSort(arr2, m);
+    printf("Array after Selection Sort:\n");
+    printArray(arr2, m);
+
+    return 0;
+}
---
+++ DSA-Assignment/CircularLinkedListOperations.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Structure for a node in the circular linked list
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create a circular linked list
+void create(struct Node** head, int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = *head;
+
+    if (*head == NULL) {
+        *head = newNode;
+        newNode->next = *head;
+    } else {
+        struct Node* temp = *head;
+        while (temp->next != *head) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+        newNode->next = *head;
+    }
+}
+
+// Function to insert a node at the beginning of the circular linked list
+void insertAtBeginning(struct Node** head, int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = *head;
+
+    if (*head == NULL) {
+        *head = newNode;
+        newNode->next = *head;
+    } else {
+        struct Node* temp = *head;
+        while (temp->next != *head) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+        newNode->next = *head;
+        *head = newNode;
+    }
+}
+
+// Function to display the circular linked list
+void displayList(struct Node* head) {
+    if (head == NULL) return;
+    struct Node* temp = head;
+    do {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    } while (temp != head);
+    printf("HEAD\n");
+}
+
+int main() {
+    struct Node* head = NULL;
+
+    // Create a circular linked list
+    create(&head, 10);
+    create(&head, 20);
+    create(&head, 30);
+    create(&head, 40);
+
+    printf("Circular Linked List after creation:\n");
+    displayList(head);
+
+    // Insert a node at the beginning
+    insertAtBeginning(&head, 5);
+
+    printf("Circular Linked List after insertion at the beginning:\n");
+    displayList(head);
+
+    return 0;
+}
